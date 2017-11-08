0c91cfb813e22207b88424b89630de1c52427089
Varun <mvarun@hotmail.co.uk>
2017-11-08 18:22:04 +0000

    Completed deaf_grandma.rb

--- chap07/deaf_grandma.rb
+++ chap07/deaf_grandma.rb
@@ -1,7 +1,19 @@
-puts "WHERE ARE MY DENTURES?!"
-    reply = gets.chomp
-if reply != reply.upcase
-  puts "HUH?!? SPEAK UP SONNY!!"
-else
-  puts "NO, NOT SINCE #{1930 + rand(21)}!"
+puts "HI SONNY!!"
+bye = 0
+
+while bye < 3
+  reply = gets.chomp
+  if reply == "BYE"
+    bye = (bye + 1)
+    if bye <= 2 
+      puts "I CANT HEAR YOU!!"
+    else
+      puts "COME BACK SOON!!"
+    end
+  elsif 
+    reply == reply.upcase
+    puts "NO, NOT SINCE #{1930 + rand(21)}!"
+  else
+    puts "HUH?!? SPEAK UP SONNY!!"
+  end
 end
